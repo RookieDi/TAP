e4846a3dbd4823b358a01066fc34796b075c97f8
vasile meghesan <mvasy03@yahoo.ro>
2024-01-03 01:17:49 -0800

    ex2 lab5

---
+++ lab5ex2/lab5 ex2/AddProduct.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5_ex2
+{
+    public partial class AddProduct : Form
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDenumire_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescriere_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtIntrare_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtIesire_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            using (MagazinDBContext mdb =new MagazinDBContext())
+            {
+                Produs p=new Produs();
+                p.Denumire = txtDenumire.Text;
+                p.DescriereProdus = txtDescriere.Text;
+                p.DataIntrare = dtIntrare.Value;
+                p.DataValabilitate=dtIesire.Value;
+
+                p.Cantitate = 1;
+
+                
+                mdb.Produse.Add(p);
+                mdb.SaveChanges();
+            }
+
+        }
+
+        private void btout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ lab5ex2/lab5 ex2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace lab5_ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.produseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adaugareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.refresh = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.cautare = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // produseToolStripMenuItem
+            // 
+            this.produseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adaugareToolStripMenuItem});
+            this.produseToolStripMenuItem.Name = "produseToolStripMenuItem";
+            this.produseToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.produseToolStripMenuItem.Text = "Produse";
+            // 
+            // adaugareToolStripMenuItem
+            // 
+            this.adaugareToolStripMenuItem.Name = "adaugareToolStripMenuItem";
+            this.adaugareToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.adaugareToolStripMenuItem.Text = "Adaugare";
+            this.adaugareToolStripMenuItem.Click += new System.EventHandler(this.adaugareToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(681, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(689, 100);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(99, 89);
+            this.refresh.TabIndex = 2;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(33, 272);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(632, 20);
+            this.txtsearch.TabIndex = 3;
+            this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
+            // 
+            // cautare
+            // 
+            this.cautare.Location = new System.Drawing.Point(715, 272);
+            this.cautare.Name = "cautare";
+            this.cautare.Size = new System.Drawing.Size(75, 23);
+            this.cautare.TabIndex = 4;
+            this.cautare.Text = "Cautare";
+            this.cautare.UseVisualStyleBackColor = true;
+            this.cautare.Click += new System.EventHandler(this.cautare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cautare);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem produseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adaugareToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Button cautare;
+    }
+}
+
