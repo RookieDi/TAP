4d4d2cd94ae63af5b0eb22a99785a8459ca07c69
RookieDi <116387505+RookieDi@users.noreply.github.com>
2024-01-03 01:30:13 -0800

    Update main.cpp

--- lab5exxx2/lab5exxx2/main.cpp
+++ lab5exxx2/lab5exxx2/main.cpp
@@ -14,7 +14,7 @@ int main() {
         std::cin >> puncte[i];
     }
 
-    // Afisare puncte 2D si distantele dintre ele
+   
     std::cout << "\nPunctele 2D introduse sunt:\n";
     for (int i = 0; i < n; ++i) {
         std::cout << "Punct " << i + 1 << ": " << puncte[i] << "\n";
